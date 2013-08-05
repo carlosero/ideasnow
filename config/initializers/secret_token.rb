887bf9fca2ffa97182ac007e45a1eda74f00c36e
Carlos Rodriguez <carlosr706@pragmagenia.com>
2013-08-05 14:37:15 -0430

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ideasnow::Application.config.secret_key_base = '3e7f34963ae1720f7d7d3ed4ea04c7781ead5ae3d9110107770b81cac320e77bf3ffbb3a943a31b9816713b0868668f318f2d13a8ef2e3f479ac14a57b0b9973'
